ca4f91702e5224e6ba19796e05684ddf4c072dfc
Kate <erma2704@yandex.ru>
2022-04-30 21:10:33 +0300

    hw3

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,32 +1,73 @@
 <template>
   <div class="home">
-    {{ text }}
-    <br/>
-    {{ arr }}
-    <MyName msg="Hello" :text="text" :obj="obj"/>
+    <header>
+      <div class="title">My personal costs</div>
+    </header>
+    <main>
+      <PaymentsDisplay :items="paymentsList"/>
+      <AddPaymentForm @addNewPayment="addPaymentData" v-show="clicked"/>
+      <br />
+      <button class="btn" @click="clicked = !clicked">ADD NEW COST +</button>
+    </main>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import PaymentsDisplay from "@/components/PaymentsDisplay.vue";
+import AddPaymentForm from "@/components/AddPaymentForm.vue";
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    MyName: HelloWorld
-  },
+    PaymentsDisplay,
+    AddPaymentForm
+},
   data() {
     return {
-      obj: {
-        text: 'asdas'
-      },
-      text: "Some text",
-      arr: [1,2,3,4,5]
-    }
+      paymentsList: [],
+      clicked: false,
+    };
   },
-}
+  methods: {
+    addPaymentData(data) {
+      this.paymentsList.push(data)
+    },
+    fetchData() {
+      return [
+        {
+          date: "28.03.2020",
+          category: "Food",
+          value: 169,
+        },
+        {
+          date: "24.03.2020",
+          category: "Transport",
+          value: 360,
+        },
+        {
+          date: "24.03.2020",
+          category: "Food",
+          value: 532,
+        },
+      ];
+    },
+  },
+  created() {
+    this.paymentsList = this.fetchData()
+  },
+  mounted() {
+  },
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
+.title {
+  font-size: 20px;
+}
+.btn{
+  background-color: lightseagreen;
+  border: none;
+  padding: 5px 10px;
+  color: white;
+  cursor: pointer;
+}
 </style>
